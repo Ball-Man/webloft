16b076dd0c51de81c1c1ea001d46360a48276558
BallMan <francesco.mistri98@gmail.com>
2020-12-31 20:24:20 +0100

    build() function instantiates template
    With the build method an actual template can be instatiated. A dist directory is created (by default 'dist/') and the produced content comes from the instantiation of the template.
    
    Currently, no files from the user config is kept. In the future, user defined images will be kept and copied with the build, so that a user will be able to insert their own images.

--- webloft/__init__.py
+++ webloft/__init__.py
@@ -1,5 +1,8 @@
 """Static HTML personal website generator."""
 import os.path as pt
+import os
+import shutil
+import glob
 
 import yaml
 import django.template as dj_template
@@ -36,3 +39,40 @@ def render(context, file='index.html', template_name='aquarius'):
     engine = dj_template.Engine(dirs=[pt.join(TEMPLATES, template_name)])
     template = engine.get_template(file)
     return template.render(context)
+
+
+def build(base_dir=pt.curdir, template_name='aquarius', dist_dir='dist',
+          templated_exts=('.html',)):
+    """Render and save an entire template, given the base dir.
+
+    The dist_dir can be specified, and will contain the HTML files (and
+    images) ready to be deployed.
+
+    Template files starting with '_' will be omitted. This makes it
+    possible to use base templates, which is a common practice in the
+    django templating workflow.
+    """
+    abs_template_files = glob.glob(pt.join(TEMPLATES, template_name, '**', '*'),
+                                   recursive=True)
+    template_files = (pt.relpath(path, pt.join(TEMPLATES, template_name))
+                      for path in abs_template_files)
+
+    # Create dist directory
+    abs_dist_dir = pt.join(base_dir, dist_dir)
+    if not pt.isdir(abs_dist_dir):
+        os.mkdir(abs_dist_dir)
+
+    # Generated build
+    for abs_file, file in zip(abs_template_files, template_files):
+        dest = pt.join(abs_dist_dir, file)
+
+        if pt.isdir(abs_file):
+            os.mkdir(dest)
+        elif pt.isfile(abs_file) \
+             and pt.splitext(file)[1].lower() in templated_exts:
+            if pt.basename(file)[0] != '_':
+                with open(dest, 'w') as fout:
+                    fout.write(render(get_context(base_dir), file,
+                               template_name))
+        elif pt.isfile(abs_file):
+            shutil.copy(abs_file, dest)
